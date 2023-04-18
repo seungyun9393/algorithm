57615cf2c20fd1eef2ac09ddd0633ce83c76f574
seungyun.chea <seungyun9393@naver.com>
2023-04-18 23:00:31 +0900

    init

---
+++ algo/src/App.java
@@ -0,0 +1,9 @@
+import chapter7.QuadTree;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        QuadTree quadTree = new QuadTree();
+
+        quadTree.slove();
+    }
+}
---
+++ algo/src/chapter7/QuadTree.java
@@ -0,0 +1,8 @@
+package chapter7;
+
+public class QuadTree {
+    
+    public void slove() {
+        System.out.println("chapter7, QuadTree slove!");
+    }
+}
