7cbe3706832de946b527c19d93ea090ed8c247d7
seungyun.chea <seungyun9393@naver.com>
2023-04-19 00:14:08 +0900

    문제해결

--- algo/src/App.java
+++ algo/src/App.java
@@ -1,9 +1,38 @@
-import chapter7.QuadTree;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        QuadTree quadTree = new QuadTree();
-
-        quadTree.slove();
-    }
+    static String s;
+	static int head = 0;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int C = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<C; i++) {
+			head = 0;
+			s = br.readLine();
+			
+			System.out.println(reverse());
+		}
+		
+	}
+	
+	static String reverse() {
+		
+		if(s.charAt(head)!='x'){
+			head++;
+			return s.charAt(head-1)+"";
+		} 
+		
+		// x이면 4가지 구역으로 쪼개짐
+		head++;
+		String str[] = new String[4];
+		str[0] = reverse(); // 왼쪽상단
+		str[1] = reverse(); // 오른쪽상단
+		str[2] = reverse(); // 왼쪽하단
+		str[3] = reverse(); // 오른쪽하단
+		
+		return "x"+str[2]+str[3]+str[0]+str[1];
+	}
 }
--- algo/src/chapter7/QuadTree.java
+++ algo/src/chapter7/QuadTree.java
@@ -2,7 +2,31 @@ package chapter7;
 
 public class QuadTree {
     
-    public void slove() {
+    char[][] pic = new char[16][16];
+
+    // 무식하게 풀어보자
+    public void slove(int x, int y, int size) {
         System.out.println("chapter7, QuadTree slove!");
+
+        // 기저사례
+        if(pic[x][y] == 'b' || pic[x][y] == 'w') {
+            for(int dx = 0; dx < size; ++dx) {
+                for(int dy=0; dy < size; ++dy) {
+                    pic[x+dx][y+dy] = 'a';
+                }
+            }
+        }
+        else {
+
+            
+            // 1. 그림 압축 풀기
+            int half = size/2;
+            
+            slove(x, y, half);              // 1사분면 0,0
+            slove(x+half, y, half);         // 2사분면 8,0
+            slove(x, y+half, half);         // 3사분면 0,8
+            slove(x+half, y+half, half);    // 4사분면 8,8
+        }
+
     }
 }
