6a48c62762bca8462624453170fa1a5bda32c1e5
seungyun.chea <seungyun.chea@e4net.net>
2023-08-29 13:42:48 +0900

    1890번문제 다시풀자

--- algo/src/dynamicProgramming/Q1890.java
+++ algo/src/dynamicProgramming/Q1890.java
@@ -27,9 +27,9 @@ public class Q1890 {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+		reverse(0, 0);
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.write(String.valueOf(reverse(0, 0)));
+		bw.write(String.valueOf(D[N][N]));
 		bw.close();
 	}
 	
@@ -37,23 +37,14 @@ public class Q1890 {
 		1. 테이블 정의
 			D[x][y] = (x, y)에서 끝 점에 도달하는 경로의 수
 		2. 점화식 찾기
-			D[x][y] = D[x+a][y] + D[x][y+a]
+			D[x][y] = D[x-a][y] + D[x][y-a]
 		3. 초기값 작성
 	*/ 
-	static Long reverse(int x, int y) {
-		if(D[x][y] != 0L) return D[x][y];
+	static void reverse(int x, int y) {
+		D[0][0] = 1L;
 
-		if(x == N-1 && y == N-1) return 1L;
+		for(int i=1; i<N; i++) {
 
-		int dx = x + map[x][y];
-		int dy = y + map[x][y];
-
-		if(dx > N && dy > N) return 0L;
-
-
-		if(dx < N) D[x][y] += reverse(dx, y);
-		if(dy < N) D[x][y] += reverse(x, dy);
-
-		return D[x][y];
+		}
 	}
 }
